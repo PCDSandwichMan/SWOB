d7ef9d6a1b1e1bd7646f9f64f725eb8a0b98fbdf
Matt <matthew.endicott46@gmail.com>
2019-11-25 21:18:37 -0700

    for heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (!dev) {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.httml"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 // ----
