6a49adb70f6179fa2722d30ab41893bb908c4df8
Matt <matthew.endicott46@gmail.com>
2019-11-25 22:07:07 -0700

    for heroku

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ if (!dev) {
   app.use(compression());
   app.use(morgan("common"));
 
-  app.use(express.static(path.resolve("client", "build")));
+  // app.use(express.static(path.resolve(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 // -----
 mongoose.connect(config.MONGODB_URI, {
